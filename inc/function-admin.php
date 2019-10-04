e58bacb739220d70077d042fc89f498724ba4b85
isheikh-dev <ishiekh.dev.v1@gmail.com>
2019-10-04 07:00:40 +0200

    create an admin page icon into the menu

---
+++ inc/function-admin.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * @package sunset-theme
+ * ======================================
+ * ADMIN PAGE
+ * ======================================
+ */
+ function sunset_add_admin_page(){
+    add_menu_page( 'Sunset Theme Options',
+                   'Sunset',
+                   'manage_options', 
+                   'alecaddd-sunset',
+                   'sunset_theme_create_page',
+                    get_template_directory_uri() . '/img/sunset-icon.png',
+                    110 
+                );
+ }
+
+add_action('admin_menu','sunset_add_admin_page' );
+
+ function sunset_theme_create_page() {
+    //   generate of our admin page
+     
+ }
